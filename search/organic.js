cdef4ff5198d9d22a3918d65fc6a6cc8197228bd
MoritzSch98 <moritzschelkle@gmail.com>
2023-11-22 14:02:49 +0100

    result css added

--- search/organic.js
+++ search/organic.js
@@ -56,29 +56,3 @@ function displayResults(results) {
         }
     });
 }
-
-
-//Show Ads
-function displayAds(ads){
-    //Create Ad Container
-    const adsContainer = document.getElementById('ad-results');
-    adsContainer.innerHTML = '';
-    //Loop ads and append them to container
-    ads.forEach(ad => {
-        //Create Ad Result Element
-        const adElement = document.createElement('div');
-        adElement.classList.add('ad');
-        adElement.innerHTML = `
-            <a href="${ad.clickout_url}" target="_blank">
-                <div id="ad-header">
-                    <img src=${escapeHtml(ad.thumbnail_url)}>
-                    <p>${escapeHtml(ad.target_url)}</p>
-                </div>
-                <h3>${escapeHtml(ad.title)} - ${escapeHtml(ad.call_to_action)}</h3>
-                <p>${escapeHtml(ad.description)}</p>
-            </a>
-        `;
-        //Append ad to container
-        adsContainer.appendChild(adElement);
-    });
-}
